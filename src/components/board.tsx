0d030cf01add22022d8327ede9a10ec0c7b8d826
anmitutarow <anmitutarow@gmail.com>
2022-05-27 20:51:52 +0900

    include Square component

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,30 +1,23 @@
 import React from "react";
 import Square from "./square";
 import useSquares from "./hooks/useSquares";
+import Container from "./style/container";
 
 const Board = () => {
   const [squares, { handleClick }] = useSquares();
   const renderSquare = (i: number) => {
     return (
       <Square
-        key={i}
+        key={i.toString()}
         value={squares[i]}
         onClick={() => handleClick(i)}
       />
     );
   }
   return (
-    <div>
-      <div className="board-row">
-        {[0, 1, 2].map(i => renderSquare(i))}
-      </div>
-      <div className="board-row">
-        {[3, 4, 5].map(i => renderSquare(i))}
-      </div>
-      <div className="board-row">
-        {[6, 7, 8].map(i => renderSquare(i))}
-      </div>
-    </div>
+    <Container>
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8].map(i => renderSquare(i))}
+    </Container>
   );
 }
 
