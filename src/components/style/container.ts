36a232308ae1a037948888f67208a6d28e4775c0
anmitutarow <anmitutarow@gmail.com>
2022-05-27 20:52:36 +0900

    designed by css grid

---
+++ src/components/style/container.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 150px;
+  height: 150px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-rows: 50px 50px 50px;
+  grid-template-columns: 50px 50px 50px;
+  border: 1px solid gray;
+`;
+
+export default Container;
