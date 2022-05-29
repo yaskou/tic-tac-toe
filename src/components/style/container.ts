cb973e83fd6bf7be6cd6b00ac91fa2912b793b2b
yaskou <anmitutarow@gmail.com>
2022-05-29 19:36:03 +0900

    width:300px

--- src/components/style/container.ts
+++ src/components/style/container.ts
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: 150px;
-  height: 150px;
+  width: 300px;
+  height: 300px;
   margin: 0 auto;
   display: grid;
-  grid-template-rows: 50px 50px 50px;
-  grid-template-columns: 50px 50px 50px;
+  grid-template-rows: 100px 100px 100px;
+  grid-template-columns: 100px 100px 100px;
   border: 1px solid gray;
 `;
 
