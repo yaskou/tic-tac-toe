37bd407031e903d0bbeb432aa5df69aa196d7ae8
yaskou <anmitutarow@gmail.com>
2022-05-30 19:56:38 +0900

    always losing AI when 3x3

---
+++ src/components/bot/lose.ts
@@ -0,0 +1,28 @@
+import { getRandomArbitrary, Min, Range } from "../tools";
+
+const lose = (
+  squares: string[] | null[],
+  lines: number[][],
+  calculateWinner: (squares: string[] | null[], lines: number[][]) => string | null
+) => {
+  if (![...squares].includes(null)) {
+    return;
+  }
+  const xo = ["X", "O"].flatMap(whois =>
+    squares.map((v, i) => v === whois && i).filter(i => i !== false)
+  ) as number[];
+  const canSet = lines.flatMap(line => {
+    const lineCopy = [...line];
+    xo.forEach(i => lineCopy.includes(i) && lineCopy.splice(lineCopy.indexOf(i), 1));
+    return lineCopy;
+  });
+  const getCanSetCount = Range(9).map(n => canSet.filter(o => o === n).length);
+  const mustSet = getCanSetCount.map((v, i) => {
+    const maybe = squares.slice();
+    return v === Min(getCanSetCount.filter(i => i > 0)) && (maybe[i] = "O", !calculateWinner(maybe, lines)) && i;
+  }).filter(i => i !== false);
+  console.log(mustSet);
+  return mustSet[getRandomArbitrary(0, mustSet.length - 1)];
+}
+
+export default lose;
