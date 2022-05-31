01f8a0a605f979d16eba17f935db21748639e829
yaskou <anmitutarow@gmail.com>
2022-05-31 21:33:25 +0900

    some style => bootstrap

--- src/components/style/container.ts
+++ src/components/style/container.ts
@@ -1,13 +1,10 @@
 import styled from "styled-components";
 
-const Container = styled.div`
+const OutLine = styled.div`
   width: 300px;
   height: 300px;
-  margin: 1em auto;
-  display: grid;
   grid-template-rows: 100px 100px 100px;
   grid-template-columns: 100px 100px 100px;
-  border: 1px solid gray;
 `;
 
-export default Container;
+export default OutLine;
