81ff5ae6295ba6403c81fe17918ef7dfd936135e
yaskou <anmitutarow@gmail.com>
2022-06-06 07:44:38 +0900

    add contorl component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import AppBar from './components/appbar';
-import Board from './components/board';
+import React from "react";
+import AppBar from "./components/appbar";
+import Board from "./components/board";
+import Control from "./components/contorol";
+import useSquares from "./hooks/useSquares";
 
 const App = () => {
+  const [squares, { clear, handleClick, result }] = useSquares();
   return (
     <>
       <AppBar brand="moku" />
       <main className="py-3">
-        <Board />
+        <Board squares={squares} handleClick={handleClick} />
+        <Control clear={clear} result={result} />
       </main>
     </>
   );
