dc7ea52719f6ed86f4430ff077c09a8352983433
yaskou <anmitutarow@gmail.com>
2022-06-08 20:27:22 +0900

    calc lines & set lines

--- src/hooks/useSquares.ts
+++ src/hooks/useSquares.ts
@@ -1,28 +1,47 @@
 import { useEffect, useState } from "react";
 import bot from "../bot";
+import { Range } from "../tools";
 
-const useSquares = () => {
-  const [squares, setSquares] = useState<(string | null)[]>(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState<boolean>(true);
-  const [message, setMessage] = useState("まだ決着はついていません");
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+const useSquares = (side: number) => {
+  side = Number(side);
+  const [squares, setSquares] = useState<(string | null)[]>(Array(9).fill(null)); // 碁番の状態を管理するフック
+  const [xIsNext, setXIsNext] = useState(true); // プレイヤーの順番を管理するフック
+  const [message, setMessage] = useState("まだ決着はついていません"); // 勝敗からメッセージを表示するためのフック
+
+  // 勝つパターンを計算する
+  const calculateLines = (side: number) => {
+    // 横のパターンを計算する
+    const across = Range(side).map(value => {
+      console.log(side)
+      return Range(side ** 2).slice(value * side, (value + 1) * side);
+    });
+    // 縦のパターンを計算する
+    const down = Range(side).map(value => {
+      return Range(side ** 2).filter(i => {
+        return i % side === value;
+      });
+    });
+    // 斜めのパターンを計算する
+    const diagonal = Range(2).map(value => {
+      return Range(side).map(i => {
+        return (side - 2 * value + 1) * (i + value);
+      });
+    });
+    // 横、縦、斜めをすべて結合する
+    return [...across, ...down, ...diagonal];
+  }
+  // パターンの状態を管理するフック
+  const [lines, setLines] = useState(calculateLines(side));
 
   // 現在の勝者がいるかどうか
-  const calculateWinner = (squares: (string | null)[]) => {
-    for (let line of lines) {
-      const [a, b, c] = line;
-      // もしパターンの一つに合うなら、そのパターンの文字を返す
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+  const calculateWinner = (squares: (string | null)[], lines: number[][]) => {
+    for (const line of lines) {
+      console.log(line)
+      const winnerIs = line.every(i => {
+        return squares[0] && squares[0] === squares[i];
+      });
+      if (winnerIs) {
+        return squares[line[0]];
       }
     }
     // でなければ、nullを返す(レンダリングされないようにするため)
@@ -32,32 +51,35 @@ const useSquares = () => {
   // 碁盤をリセットする
   const reset = () => {
     setSquares(Array(9).fill(null)); // マス目をnullで埋める(レンダリングされない)
-    setXIsNext(true); // プレイヤーを先手にする
+    setXIsNext(true); // 人間を先手にする
   }
 
   // マス目がクリックされたときの処理 
   const handleClick = (i: number) => { // iはクリックされたマス目のインデックス
     const newSquares = [...squares]; // 碁盤のコピーを作成する
     // もし勝者がすでにいる、またはマス目がすでに埋められているなら、関数を抜ける
-    if (calculateWinner(newSquares) || newSquares[i]) {
+    if (calculateWinner(newSquares, lines) || newSquares[i]) {
       return;
     }
     newSquares[i] = xIsNext ? "X" : "O"; // コピーした配列のi番目を現在のプレイヤーにする
     setSquares(newSquares); // コピーした配列を碁番にセットする
-    setXIsNext(!xIsNext); // プレイヤーを反転する
+    setXIsNext(!xIsNext); // 現在のプレイヤーを反転する
   }
 
   // 碁盤の状態が変わったとき
   useEffect(() => {
     // 勝者がいた場合に、勝者によって表示する内容を変える
-    switch (calculateWinner(squares)) {
+    switch (calculateWinner(squares, lines)) {
       // 勝者が人間の場合
       case "X":
-        setMessage("おやっ、あなたは勝ってしまいなした");
+        setMessage("おやっ、あなたは勝ってしまいました");
         break;
       // 勝者がAIの場合
       case "O":
         setMessage("あなたに勝ってしまいました");
+        break;
+      default:
+        setMessage("まだ決着はついていません");
         break;
     }
     // eslint-disable-next-line
@@ -68,12 +90,16 @@ const useSquares = () => {
     // もしプレイヤーがAIになったとき
     if (!xIsNext) {
       // AIからの戻り値のマスを埋める
-      const responce = bot(squares,lines,calculateWinner) as number; // 型にundefinedが含まれるため、型アサーション
+      const responce = bot(squares, lines, side, calculateWinner) as number; // 型にundefinedが含まれるため、型アサーション
       handleClick(responce);
     }
     // eslint-disable-next-line
   }, [xIsNext]);
-  
+
+  useEffect(() => {
+    setLines(calculateLines(side));
+  }, [side]);
+
   return [squares, message, { handleClick, reset }] as const;
 }
 
