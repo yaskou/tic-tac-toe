92eb6f74117abba421bec14f88d065c1bc96d201
yaskou <anmitutarow@gmail.com>
2022-06-08 20:26:21 +0900

    context side

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { createContext } from "react";
 import AppBar from "./components/appbar";
 import Board from "./components/board";
 import Control from "./components/contorol";
+import useSide from "./hooks/useSide";
 import useSquares from "./hooks/useSquares";
 
+export const sideContext = createContext(3);
+
 const App = () => {
-  const [squares, { clear, handleClick, result }] = useSquares();
+  const [side, { handleChange }] = useSide();
+  const [squares, message, { handleClick, reset }] = useSquares(side);
   return (
-    <>
+    <sideContext.Provider value={side}>
       <AppBar brand="moku" />
       <main className="py-3">
         <Board squares={squares} handleClick={handleClick} />
-        <Control clear={clear} result={result} />
+        <Control message={message} reset={reset} handleChange={handleChange} />
       </main>
-    </>
+    </sideContext.Provider>
   );
 }
 
