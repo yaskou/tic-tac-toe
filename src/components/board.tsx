264fae0411f31738a662e467709fb936175d9049
yaskou <anmitutarow@gmail.com>
2022-06-08 20:26:52 +0900

    useContext side

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,8 +1,9 @@
 /** @jsxImportSource @emotion/react */
-import React from "react";
+import React, { useContext } from "react";
 import Square from "./square";
 import { Range } from "../tools";
 import grid from "./style/grid";
+import { sideContext } from "../App";
 
 interface Props {
   squares: (string | null)[],
@@ -10,12 +11,13 @@ interface Props {
 }
 
 const Board = ({ squares, handleClick }: Props) => {
+  const side = useContext(sideContext);
   return (
     <div
       className="d-grid border border-success mx-auto"
-      css={grid(300, 3)}
+      css={grid(300, side)}
     >
-      {Range(9).map(i => <Square value={squares[i]} key={i} onClick={() => handleClick(i)} />)}
+      {Range(side ** 2).map(i => <Square value={squares[i]} key={i} onClick={() => handleClick(i)} />)}
     </div>
   );
 }
